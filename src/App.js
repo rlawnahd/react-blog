d1659b3e3323c78f3bc6669412acc7a9379c9d89
rlawnahd <rlawnahd123@naver.com>
2023-03-13 23:44:14 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [logo, setLogo] = useState("ReactBlog");
+  let [title, setTitle] = useState("강남 우동 맛집");
+  let [title2, setTitle2] = useState("강남 우동");
+  let [title3, setTitle3] = useState("강남");
+  let [따봉, c] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>{logo}</h4>
+      </div>
+      <div className="list">
+        <h4>
+          {title} <span onClick={{}}>좋아요</span>
+          {따봉}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>
+          {title2}
+          <span>좋아요</span>
+          {따봉}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>
+          {title3}
+          <span>좋아요</span>
+          {따봉}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
